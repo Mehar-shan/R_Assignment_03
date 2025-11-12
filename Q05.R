0da86d551f091857bf69a12d79f7e8fb324e0423
Mehar-shan <meharalimeharali890@gmail.com>
2025-11-12 00:40:04 -0800

    soloution for Q05

---
+++ Q05.R
@@ -0,0 +1,31 @@
+# Q5: Load libraries
+library(DBI)
+library(RMariaDB)
+library(data.table)
+
+# Connect to MySQL
+con <- dbConnect(
+  RMariaDB::MariaDB(),
+  user = "root",
+  password = "mysql@576",
+  dbname = "sakila",
+  host = "localhost",
+  port = 3306
+)
+
+# Load payment and staff tables
+payment <- as.data.table(dbReadTable(con, "payment"))
+staff   <- as.data.table(dbReadTable(con, "staff"))
+
+# Close connection
+dbDisconnect(con)
+
+# Q5 query using data.table
+payment_staff <- merge(payment, staff, by = "staff_id", all.x = TRUE)
+payment_info <- payment_staff[, .(
+  payment_id,
+  amount,
+  payment_date,
+  staff_name = paste(first_name, last_name)
+)]
+print(payment_info)
